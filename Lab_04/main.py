fa2d760338a7a8e4c8b44c6b2e82177d961f790b
Rafael Isaac Cano Guitton <rafael.cano@ucsp.edu.pe>
2021-06-03 20:15:13 -0500

    Add files via upload

---
+++ Lab_04/main.py
@@ -0,0 +1,14 @@
+from dependencies.data import Perceptron
+import time
+
+digitos = Perceptron()
+digitos.showPatterns()
+
+digitos.TestFromImg("Patterns/test/pat32.png")
+digitos.TestFromImg("Patterns/test/pat33.png")
+digitos.TestFromImg("Patterns/test/pat34.png")
+
+digitos.TestFromImg("Patterns/test/pat41.png")
+digitos.TestFromImg("Patterns/test/pat44.png")
+
+digitos.TestFromImg("Patterns/test/pat8.png")
